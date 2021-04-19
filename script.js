fef6a2aac92ac8b1a3890d49aed79ed995267537
JouPimentta <josue.pimentta@gmail.com>
2021-04-19 13:08:38 -0300

    add code to close the images

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // selecting all required elements
 const gallery = document.querySelectorAll('.grid-container .images'),
-previewBox = document.querySelector('.background');
+previewBox = document.querySelector('.background'),
+closeIcon = document.querySelector('.x-icon');
 
 window.onload = () => {
     for (let i = 0; i < gallery.length; i++){
         gallery[i].onclick = ()=>{
             console.log(i);
             previewBox.classList.add("show");
+
+            closeIcon.onclick = ()=>{                
+                previewBox.classList.remove("show");
+            }
         }
     }
 }
