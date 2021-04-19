85d3760dccfa55c50da63db73b4b9a0573359686
JouPimentta <josue.pimentta@gmail.com>
2021-04-19 13:03:42 -0300

    add code to open the images

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // selecting all required elements
-const gallery = document.querySelector('.gallery'),
+const gallery = document.querySelectorAll('.grid-container .images'),
 previewBox = document.querySelector('.background');
+
+window.onload = () => {
+    for (let i = 0; i < gallery.length; i++){
+        gallery[i].onclick = ()=>{
+            console.log(i);
+            previewBox.classList.add("show");
+        }
+    }
+}
