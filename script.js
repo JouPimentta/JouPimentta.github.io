c1d69a8f1c6c033d2bd09d7d289208ca7dadc867
JouPimentta <josue.pimentta@gmail.com>
2021-04-19 21:15:22 -0300

    makes the next and previous buttons work

--- script.js
+++ script.js
@@ -6,18 +6,58 @@ closeIcon = previewBox.querySelector('.x-icon');
 
 window.onload = () => {
     for (let i = 0; i < images.length; i++){
+        let newIndex = i;
+        let clickImgIndex;
         images[i].onclick = ()=>{
+            clickImgIndex = newIndex;
             console.log(i);            
                 function preview(){
-                    let imageUrl = images[i].querySelector('img').src;
+                    let imageUrl = images[newIndex].querySelector('img').src;
                     previewImg.src = imageUrl;
                     console.log(imageUrl);
                 }
-            previewBox.classList.add("show");     
-            closeIcon.onclick = ()=>{                
+                //previous and next button
+                const prevBtn = document.querySelector(".prev");
+                const nextBtn = document.querySelector(".next");
+                if (newIndex == 0) {
+                    prevBtn.style.display = "none";                    
+                }
+                if (newIndex >= images.length - 1) {
+                    nextBtn.style.display = "none";                    
+                }
+                prevBtn.onclick = ()=>{
+                    newIndex--;
+                    if (newIndex == 0) {
+                        preview();
+                        prevBtn.style.display = "none";
+                        nextBtn.style.display = "block";
+                    } else {
+                        preview();
+                        prevBtn.style.display = "block";
+                        nextBtn.style.display = "block"; 
+                    }
+                }
+                nextBtn.onclick = ()=>{
+                    newIndex++;
+                    if (newIndex >= images.length - 1) {
+                        preview();
+                        nextBtn.style.display = "none";
+                        prevBtn.style.display = "block";
+                    } else {
+                        preview();
+                        prevBtn.style.display = "block";
+                        nextBtn.style.display = "block"; 
+                    }
+                }
+            previewBox.classList.add("show");   
+
+            closeIcon.onclick = ()=>{       
+                clickImgIndex = newIndex;
+                prevBtn.style.display = "block";
+                nextBtn.style.display = "block";         
                 previewBox.classList.remove("show");
-            }
-            preview();               
+            }     
+            preview();      
         }
     }
 }
